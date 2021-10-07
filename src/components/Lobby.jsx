60417ab9d7e3b5b5429010d764a6003e7f77f5b7
MaJesus <mareyes.esp@gmail.com>
2021-10-07 12:34:20 -0300

    all over again

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import "../components/css/lobby.css";
 
 export const Lobby = () => {
-return (
-    <div className="container">
-        <h1>LOBBY</h1>
+    let history = useHistory();
+
+    return(
+    <div class='container'>
+    
+    <div className='page_container'>
+    <div className='left_container'>
+        <button onClick={() => { history.push('/waiter') }} id='waiter_btn' className='lobby_btn'> W A I T E R </button>
+        <button onClick={() => { history.push('/kitchen') }} id='kitchen_btn' className='lobby_btn'> K I T C H E N </button>
     </div>
-)
+    <div className='right_container'>
+        <img className='lobby_logo' src={require('./img/logo.png').default} alt='logo' />
+        <p>THE HEARTLESS BISTRO</p>
+    </div>
+    </div>
+
+    </div>
+    )
 };
 
 export default Lobby;
