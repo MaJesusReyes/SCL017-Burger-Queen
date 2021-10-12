c0d8185919027819ad2cdc166d80a8be35e9adb5
MaJesus <mareyes.esp@gmail.com>
2021-10-12 13:51:57 -0300

    I dont wanna cry but i will anyways

--- src/components/Waiter.jsx
+++ src/components/Waiter.jsx
@@ -15,42 +15,45 @@ export const Waiter = () => {
         setMenu(data.lunch)
     };
 
+    const displayDrinks = () => {
+      setMenu(data.drinks)
+    };
 
     return (
       <div className='page_Container'>
 
       <div className='sidebar'>
         <button  onClick={displayBreakfast} className='sidebar_btn'>
-          <img src={'https://i.imgur.com/tOjFDWm.png'} className='sidebar_img' alt='breakfast'/>
+          <img src={'https://i.imgur.com/qGyVthl.png'} className='sidebar_img' alt='breakfast'/>
         </button>
         <button onClick={displayLunch} className='sidebar_btn'>
-          <img src={'https://i.imgur.com/sBN1daL.png'} className='sidebar_img' alt='lunch'/>
+          <img src={'https://i.imgur.com/eW2dnMw.png'} className='sidebar_img' alt='lunch'/>
+        </button>
+        <button onClick={displayDrinks} className='sidebar_btn'>
+          <img src={'https://i.imgur.com/ar3eSMo.png'} className='sidebar_img' alt='drinks'/>
         </button>
       </div>
 
       <div className='menu_container'>
-      
         {menu.map((item) => (
-          <div key={item.id} className="cardMenu ">
+          <div key={item.id} className="card-menu">
+
                 <div className="img-container">
                   <div className="img-inner">
                     <div className="inner-skew">
-                      <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"/>
+                      <img src={item.image}/>
                     </div>
                   </div>
                 </div>
 
                 <div className="card-body">
                   <h5 className="card-title">{item.name}</h5>
+                  <p className="card-description">{item.description}</p>
                   <p className="card-price">${item.price}</p>
-                  
                 </div>
           
           </div>
         ))};
-        
-        
-
       </div>
         
 
