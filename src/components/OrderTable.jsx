b5108f5a9cb379d3cca5927c4a52e46ac127c670
MaJesus <mareyes.esp@gmail.com>
2021-10-14 21:50:05 -0300

    Order-table CSS ready

--- src/components/OrderTable.jsx
+++ src/components/OrderTable.jsx
@@ -7,13 +7,16 @@ export const OrderTable = (props) => {
 return(
     <div className='orderTable'>
         <h1>NEW ORDER!</h1>{'\n'} 
+        <div className='table_orders'>
         {cart.map((item) => (
-            <div key={item.id} className='petition_table'>
+            <div key={item.id} className='individual-orders'>
                 {`${item.name} ${item.quantity} = $${item.price}`}
                 <input type="submit" value="X" className='delete_btn' onClick={()=>onRemove(item)}/>        
             </div>
         ))}
+        </div>
         <div className='total_div'>Total: ${cartTotal}</div>
+        <button className='send_btn'> SEND ORDER TO KITCHEN </button>
 
     </div>
 
